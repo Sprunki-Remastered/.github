762d6fd5125ea01f3e2680a9cb31ab778e5b56db
Haland James <conbongu4@outlook.com>
2024-10-30 14:48:19 +0700

    Create index.js

---
+++ profile/index.js
@@ -0,0 +1,8 @@
+// index.js - Main entry point for Sprunki Remastered
+
+const initializeGame = () => {
+  console.log("Welcome to Sprunki Remastered!");
+  // Game initialization code goes here.
+};
+
+initializeGame();
